46deac60add10226be21fa4a3d47f0906fffb05c
Anuradha <anuradhakandekar07@gmail.com>
2019-08-01 20:48:33 +0530

    commiting my first java class

---
+++ BasicCoreJava/src/variablesanddatatyps/FirstJavaClass.java
@@ -0,0 +1,12 @@
+package variablesanddatatyps;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi anuradha");
+		// TODO Auto-generated method stub
+
+	}
+
+}
