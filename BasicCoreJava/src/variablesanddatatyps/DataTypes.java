e7c164f03e2d1abe7ead4ba61ce35e50e90eea1e
Anuradha <anuradhakandekar07@gmail.com>
2019-08-01 21:25:17 +0530

    commiting Data types

---
+++ BasicCoreJava/src/variablesanddatatyps/DataTypes.java
@@ -0,0 +1,36 @@
+package variablesanddatatyps;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//integer
+		int age=24;
+		System.out.println(age);
+		
+		//charcter
+		char gender='F';
+		
+		//decimal values
+		double marks=75;
+		float grade=7.5f;
+		
+		//marital status
+		boolean status=true;
+		
+		//strings
+		String name="Anuradha";
+		
+		//for integer values
+		byte a=10;
+		short b=20;
+		int c= 30;
+		long d= 40;
+		
+		
+		
+
+	}
+
+}
