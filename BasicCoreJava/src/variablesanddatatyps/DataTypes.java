e6bd0a05e0da74d708e6d8036b0809585688a3f7
Anuradha <anuradhakandekar07@gmail.com>
2019-08-07 21:47:26 +0530

    Data type

--- BasicCoreJava/src/variablesanddatatyps/DataTypes.java
+++ BasicCoreJava/src/variablesanddatatyps/DataTypes.java
@@ -7,7 +7,7 @@ public class DataTypes {
 		
 		//integer
 		int age=24;
-		System.out.println(age);
+		
 		
 		//charcter
 		char gender='F';
@@ -27,6 +27,17 @@ public class DataTypes {
 		short b=20;
 		int c= 30;
 		long d= 40;
+		
+		System.out.println("Age is    = "+age);
+		System.out.println("gender is = "+gender);
+		System.out.println("marks is  = "+marks);
+		System.out.println("grade is  = "+grade);
+		System.out.println("status is = "+status);
+		System.out.println("name is   = "+name);
+		System.out.println("a is      = "+a);
+		System.out.println("b is      = "+b);
+		System.out.println("c is      = "+c);
+		System.out.println("d is      = "+d);
 		
 		
 		
